4114a3bdb0dca2fe5c97f1b910e82b44ec828793
MiraFinkel <mirafinkel@gmail.com>
2021-02-15 17:56:55 +0200

    added the project
    added the project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private wrmhlRead wrmhl;
+    [SerializeField] private GameObject cube;
+    [SerializeField] private float speed;
+
+
+    void Update()
+    {
+        string data = wrmhl.getData();
+        if (data != null)
+        {
+            Dictionary<string, float> floatDataArr = parseData(data);
+            cube.transform.localEulerAngles += new Vector3(floatDataArr["xNorm"], floatDataArr["yNorm"], floatDataArr["zNorm"]) * speed * Time.deltaTime;
+        }
+    }
+
+    Dictionary<string, float> parseData(string data)
+    {
+        //Debug.Log("string" + data);
+        string[] dataArr = data.Split('.');
+        float xRaw;
+        float yRaw;
+        float zRaw;
+        float xNorm;
+        float yNorm;
+        float zNorm;
+        if (float.TryParse(dataArr[0], out xRaw))
+        {
+            Debug.Log("xRaw: " + xRaw);
+        }
+        if (float.TryParse(dataArr[1], out yRaw))
+        {
+            Debug.Log("yRaw: " + yRaw);
+        }
+        if (float.TryParse(dataArr[0], out zRaw))
+        {
+            Debug.Log("zRaw: " + zRaw);
+        }
+        if (float.TryParse(dataArr[0], out xNorm))
+        {
+            Debug.Log("xNorm: " + xNorm);
+        }
+        if (float.TryParse(dataArr[0], out yNorm))
+        {
+            Debug.Log("yNorm: " + yNorm);
+        }
+        if (float.TryParse(dataArr[0], out zNorm))
+        {
+            Debug.Log("zNorm: " + zNorm);
+        }
+        Dictionary<string, float> floatDataArr = new Dictionary<string, float>();
+        floatDataArr.Add("xRaw", xRaw);
+        floatDataArr.Add("yRaw", yRaw);
+        floatDataArr.Add("zRaw", zRaw);
+        floatDataArr.Add("xNorm", xRaw);
+        floatDataArr.Add("yNorm", yNorm);
+        floatDataArr.Add("zNorm", zNorm);
+        return floatDataArr;
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private float mSpeed = 2f;
+    public float mRotSpeed = 120;
+    public float mRotation = 0f;
+    public float mGravity = 8;
+
+    private Vector3 mMoveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.transform.position.Set(1.5f, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            mMoveDirection = new Vector3(0, 0, 1);
+            mMoveDirection *= mSpeed;
+            mMoveDirection = transform.TransformDirection(mMoveDirection);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            mMoveDirection = new Vector3(0, 0, -1);
+            mMoveDirection *= mSpeed;
+            mMoveDirection = transform.TransformDirection(mMoveDirection);
+        }
+        //if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+        //{
+        //    ChickenStopMove();
+        //}
+        mRotation += Input.GetAxis("Horizontal") * mRotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, mRotation, 1);
+
+        mMoveDirection.y -= mGravity * Time.deltaTime;
+    }
+}
