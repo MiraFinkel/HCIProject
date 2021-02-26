c89f7e02df2970bd91a550ae3211261517781cb8
EliorOmer <79097594+EliorOmer@users.noreply.github.com>
2021-02-26 15:15:24 +0200

    orbit motion
    kanal

---
+++ Assets/OrbitMotion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMotion : MonoBehaviour
+{
+    public Transform orbittinObject;
+    public Ellipse orbitPath;
+
+    [Range(0f,1f)]
+    public float orbitProgrss = 0f;
+    public float orbitPeriod = 3f;
+    public bool orbitActive = true;
+
+    void Start()
+    {
+        if (orbittinObject == null)
+        {
+            orbitActive = false;
+            return;
+        }
+        SetOrbitingObjectPositiion();
+        // if (orbit is active) stsart orbit 
+    }
+
+    void SetOrbitingObjectPositiion()
+    {
+        Vector2 orbitPos = orbitPath.Evaluate(orbitProgrss);
+        orbittinObject.localPosition = new Vector3(orbitPos.x, 0, orbitPos.y);
+    }
+
+    IEnumerator AnimateOrbit()
+    {
+        if (orbitPeriod < 0.1f)
+        {
+            orbitPeriod = 0.1f;
+        }
+        float orbitSpeed = 1f / orbitPeriod;
+
+        while (orbitActive)
+        {
+            orbitProgrss += Time.deltaTime * orbitSpeed;
+            orbitProgrss %= 1f;
+            SetOrbitingObjectPositiion();
+            yield return null;
+        }
+    }
+
+}
