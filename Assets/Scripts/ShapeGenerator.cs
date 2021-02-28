1cf5cf756aafc5998f785aecf9fe891cd23266d7
MiraFinkel <mirafinkel@gmail.com>
2021-02-28 19:55:01 +0200

    generate shapes
    generate shapes

--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -5,37 +5,47 @@ using UnityEngine;
 public class ShapeGenerator : MonoBehaviour
 {
     [SerializeField] private Transform generatorPosition;
-    [SerializeField] private int shapeAmount = 1;
+    //[SerializeField] private int shapeAmount = 1;
+    [SerializeField] private float respawnTime = 1.0f;
+    [SerializeField] private bool spawn = false;
 
-    [Range(3, 10)]
-    [SerializeField] private int numOfSquers = 3;
-    [Range(0, 1)]
-    [SerializeField] private float scale = 1f;
-    [Range(0, 1)]
-    [SerializeField] private float red;
-    [Range(0, 1)]
-    [SerializeField] private float green;
-    [Range(0, 1)]
-    [SerializeField] private float blue;
+    //[Range(3, 10)]
+    //[SerializeField] private int numOfSquers = 3;
+    //[Range(0f, 1f)]
+    //[SerializeField] private float scale = 1f;
+    //[Range(0f, 1f)]
+    //[SerializeField] private float red;
+    //[Range(0f, 1f)]
+    //[SerializeField] private float green;
+    //[Range(0f, 1f)]
+    //[SerializeField] private float blue;
 
     void Start()
     {
-        for (int i = 0; i < shapeAmount; i++)
-        {
-            GenerateShape();
-        }
+        StartCoroutine(GenerateShapes());
     }
 
-    // Update is called once per frame
-    void GenerateShape()
+    IEnumerator GenerateShapes()
+    {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            GenerateShape();
+        }
+        
+    }
+
+    private void  GenerateShape()
     {
         GameObject shapeGO = Instantiate(Resources.Load("Shape")) as GameObject;
         shapeGO.transform.position = generatorPosition.position;
+
         VoxelRenderer voxelRenderer = shapeGO.GetComponent<VoxelRenderer>();
-        voxelRenderer.numOfSquers = numOfSquers;
-        voxelRenderer.scale = scale;
-        voxelRenderer.red = red;
-        voxelRenderer.green = green;
-        voxelRenderer.blue = blue;
+
+        voxelRenderer.numOfSquers = Random.Range(3, 10);
+        voxelRenderer.scale = Random.Range(0f, 1f);
+        voxelRenderer.red = Random.Range(0f, 1f);
+        voxelRenderer.green = Random.Range(0f, 1f);
+        voxelRenderer.blue = Random.Range(0f, 1f);
     }
 }
